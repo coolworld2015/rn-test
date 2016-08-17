5196069c30861ac2d22ff3f15336792cafda715f
admin <admin>
2016-08-17 14:57:10 +0300

    init

---
+++ login.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+
+
+var Text = ReactNative.Text;
+var StyleSheet = ReactNative.Text;
+
+var Login = React.createClass({
+  render: function() {
+      return (
+          <Text style={{color: 'red'}}>Hello</Text>
+      )
+  }
+});
+
+module.exports = Login;
